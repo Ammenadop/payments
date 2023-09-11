72c4782ffe0cce9c8a678217c505b43aa8aaf810
Ammenadop <kiftkinz49@gmail.com>
2023-09-11 15:59:08 +0530

    change espense

--- ccavResponseHandler.js
+++ ccavResponseHandler.js
@@ -23,7 +23,7 @@ exports.postRes = function (request, response) {
     htmlcode = '<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><title>Response Handler</title></head><body><center><font size="4" color="blue"><b>Response Page</b></font><br>'+ pData +'</center><br></body></html>';
     // response.writeHeader(200, {"Content-Type": "text/html"});
     // response.write(htmlcode);
-    response.status(200).send(ccavEncResponse);
+    response.status(200).send(ccavResponse);
     response.end();
 }); 
 };
