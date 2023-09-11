d72395828449a97b0f74435488218bc594a37ee8
Ammenadop <kiftkinz49@gmail.com>
2023-09-11 16:31:45 +0530

    change response

--- ccavResponseHandler.js
+++ ccavResponseHandler.js
@@ -14,16 +14,14 @@ exports.postRes = function (request, response) {
     ccavResponse = ccav.decrypt(encryption, workingKey);
   });
 
-  request.on('end', function () {
-    var pData = '';
-    pData = '<table border=1 cellspacing=2 cellpadding=2><tr><td>'	
-    pData = pData + ccavResponse.replace(/=/gi,'</td><td>')
-    pData = pData.replace(/&/gi,'</td></tr><tr><td>')
-    pData = pData + '</td></tr></table>'
-    htmlcode = '<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><title>Response Handler</title></head><body><center><font size="4" color="blue"><b>Response Page</b></font><br>'+ pData +'</center><br></body></html>';
-    // response.writeHeader(200, {"Content-Type": "text/html"});
-    // response.write(htmlcode);
-    response.status(200).send(ccavResponse);
+  request.on("end", function () {
+    const keyValuePairs = ccavResponse.split("&");
+    var result = {};
+    for (const pair of keyValuePairs) {
+      const [key, value] = pair.split("=");
+      result[key] = value;
+    }
+    response.status(200).send(result);
     response.end();
-}); 
+  });
 };
