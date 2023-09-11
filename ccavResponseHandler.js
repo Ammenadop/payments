4412123dc9401b7721e43469f67b05786850efc5
Ammenadop <kiftkinz49@gmail.com>
2023-09-11 15:54:48 +0530

    change espense

--- ccavResponseHandler.js
+++ ccavResponseHandler.js
@@ -20,9 +20,10 @@ exports.postRes = function (request, response) {
     pData = pData + ccavResponse.replace(/=/gi,'</td><td>')
     pData = pData.replace(/&/gi,'</td></tr><tr><td>')
     pData = pData + '</td></tr></table>'
-          htmlcode = '<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><title>Response Handler</title></head><body><center><font size="4" color="blue"><b>Response Page</b></font><br>'+ pData +'</center><br></body></html>';
-          response.writeHeader(200, {"Content-Type": "text/html"});
-    response.write(htmlcode);
+    htmlcode = '<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><title>Response Handler</title></head><body><center><font size="4" color="blue"><b>Response Page</b></font><br>'+ pData +'</center><br></body></html>';
+    // response.writeHeader(200, {"Content-Type": "text/html"});
+    // response.write(htmlcode);
+    response.status(200).send(ccavEncResponse);
     response.end();
 }); 
 };
